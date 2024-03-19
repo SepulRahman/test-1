ba4f0583d83857c8d512ae1e0f6207dd0db85ffd
P340 <saifulrahmansalinderi@gmail.com>
2024-03-19 12:08:00 +0800

    first commit

---
+++ app/src/main/java/com/example/exercise1_05ddt21f2020/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.exercise1_05ddt21f2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+
+//        textView = findViewById(R.id.textView);
+//        button   = findViewById(R.id.button);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                textView.setText("Welcome to DFP50293");
+//            }
+//        });
+
+    }
+}
